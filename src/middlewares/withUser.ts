a75650887ebad2071e3fd1ec97be66787a9a6267
Estifanos <95101485+justEstif@users.noreply.github.com>
2024-11-29 08:11:08 -0500

    Fix redirect

--- src/middlewares/withUser.ts
+++ src/middlewares/withUser.ts
@@ -3,8 +3,9 @@ import { pbServer } from "@/lib/pb";
 
 export const withUser = async (request: NextRequest) => {
   const pb = await pbServer(request);
+  console.log("Model in withUser", pb.authStore.model);
   if (!pb.authStore.model) {
-    return NextResponse.redirect("/");
+    return NextResponse.redirect(new URL("/", request.url));
   }
   return NextResponse.next();
 };
