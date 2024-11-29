ca1d115772879944ce25ab15612c0fbf87d0329c
Estifanos <95101485+justEstif@users.noreply.github.com>
2024-11-29 08:11:13 -0500

    add logging middleware

---
+++ src/middlewares/withLogging.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const withLogging = (request: NextRequest) => {
+  const { pathname } = request.nextUrl;
+
+  // Exclude static files and API routes from processing
+  if (
+    !(
+      pathname.startsWith("/_next/") ||
+      pathname.startsWith("/favicon.ico") ||
+      pathname.startsWith("/static/")
+    )
+  ) {
+    console.log(
+      `[${new Date().toISOString()}] ${request.method} ${request.url}`,
+    );
+  }
+
+  return NextResponse.next();
+};
