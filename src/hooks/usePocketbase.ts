5e17a27bb9463d7bafe58ec2942004f7cb4da51f
Estifanos <95101485+justEstif@users.noreply.github.com>
2024-12-01 16:56:28 -0500

    Set the oauth provider's avatar

--- src/hooks/usePocketbase.ts
+++ src/hooks/usePocketbase.ts
@@ -27,6 +27,20 @@ const usePocketbase = () => {
       const authData = await pb
         .collection("users")
         .authWithOAuth2({ provider: "github" });
+
+      const meta = authData.meta || {};
+      if (meta.isNew) {
+        const formData = new FormData();
+        const response = await fetch(meta.avatarUrl);
+        if (response.ok) {
+          const file = await response.blob();
+          formData.append("avatar", file);
+        }
+
+        formData.append("name", meta.name);
+        await pb.collection("users").update(authData.record.id, formData);
+      }
+
       setUser(pb.authStore.model);
       if (typeof window !== "undefined") {
         document.cookie = pb.authStore.exportToCookie({ httpOnly: false });
