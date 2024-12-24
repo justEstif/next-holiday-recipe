e1ad7913c74a437b3a787115fe3ad051a4cff966
estifanos <95101485+justEstif@users.noreply.github.com>
2024-12-24 06:21:13 -0500

    feat: add biome, remove eslint

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8090",
-        pathname: "/api/files/**",
-      },
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "127.0.0.1",
+				port: "8090",
+				pathname: "/api/files/**",
+			},
+		],
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 };
 
 export default nextConfig;
